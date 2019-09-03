11c0bb431d5996aba16f4a3bda5a955bb222edcf
Nick Strohm <nick.strohm99@gmail.com>
2019-09-03 23:20:56 +0200

    Add thefuck executable

--- init.sh
+++ init.sh
@@ -2,7 +2,10 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt-get install -y git wget curl zsh
+sudo apt install python3-dev python3-pip
+sudo pip3 install thefuck
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
+source ~/.zshrc

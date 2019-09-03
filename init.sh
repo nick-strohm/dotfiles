00c7dfe36711e9474446ef4a31c3c44fa094164b
Nick Strohm <nick.strohm99@gmail.com>
2019-09-03 23:10:23 +0200

    Add powerline10k dotfile

--- init.sh
+++ init.sh
@@ -4,4 +4,5 @@ sudo apt-get dist-upgrade -y
 sudo apt-get install -y git wget curl zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-mv .zshrc ~/.zshrc
+cp .zshrc ~/.zshrc
+cp .p10k.zsh ~/.p10k.zsh
